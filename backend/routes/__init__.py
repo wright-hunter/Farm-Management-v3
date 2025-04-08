52e940080debea2b316ddc16e669c78ca24a99cc
Hunter Wright <hunter.wright.sd@gmail.com>
2025-04-08 00:30:04 -0500

    registered routes for equip and expenses

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,5 +1,9 @@
 # Import and register blueprints here
 from routes.fields import fields_bp
+from routes.equipment import equipment_bp
+from routes.yearly_expenses import yearly_expenses_bp
 
 def register_blueprints(app):
     app.register_blueprint(fields_bp)
+    app.register_blueprint(equipment_bp)
+    app.register_blueprint(yearly_expenses_bp)
